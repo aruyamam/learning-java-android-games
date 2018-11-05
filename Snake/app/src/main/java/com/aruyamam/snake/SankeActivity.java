77a67b2b5662597528c64772721b1a313ce803d4
aruyamam <aruyamam@gmail.com>
2018-11-05 11:44:00 +0900

    Snake game first commit

---
+++ Snake/app/src/main/java/com/aruyamam/snake/SankeActivity.java
@@ -0,0 +1,12 @@
+package com.aruyamam.snake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SankeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
