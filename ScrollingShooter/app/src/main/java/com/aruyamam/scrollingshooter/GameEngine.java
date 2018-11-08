33df49c25636d575e834adae4ff581d347438c3c
aruyamam <aruyamam@gmail.com>
2018-11-08 20:18:07 +0900

    Coding GameState & SoundEngine class

--- ScrollingShooter/app/src/main/java/com/aruyamam/scrollingshooter/GameEngine.java
+++ ScrollingShooter/app/src/main/java/com/aruyamam/scrollingshooter/GameEngine.java
@@ -6,27 +6,38 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 
-class GameEngine extends SurfaceView implements Runnable {
+class GameEngine extends SurfaceView implements Runnable, GameStarter {
 
     private Thread mThread = null;
     private long mFPS;
 
+    private GameState mGameState;
+    private SoundEngine mSoundEngine;
+
     public GameEngine(Context context, Point size) {
         super(context);
 
+        mGameState = new GameState(this, context);
+        mSoundEngine = new SoundEngine(context);
     }
 
     @Override
     public void run() {
-        long frameStartTime = System.currentTimeMillis();
-        // Update all the game objects here
-        // Draw all the game objects here
+        while (mGameState.getThreadRunning()) {
+            long frameStartTime = System.currentTimeMillis();
 
-        // Measure the  frames per second in the usual way
-        long timeThisFrame = System.currentTimeMillis() - frameStartTime;
-        if (timeThisFrame >= 1) {
-            final int MILLIS_IN_SECOND = 1000;
-            mFPS = MILLIS_IN_SECOND / timeThisFrame;
+            if (!mGameState.getPaused()) {
+                // Update all the game objects here
+            }
+
+            // Draw all the game objects here
+
+            // Measure the frames per second
+            long timeThisFrame = System.currentTimeMillis() - frameStartTime;
+            if (timeThisFrame >= 1) {
+                final int MILLIS_IN_SECOND = 1000;
+                mFPS = MILLIS_IN_SECOND / timeThisFrame;
+            }
         }
     }
 
@@ -34,10 +45,15 @@ class GameEngine extends SurfaceView implements Runnable {
     public boolean onTouchEvent(MotionEvent motionEvent) {
         // Handle the player's input
 
+        // testing
+        mSoundEngine.playShoot();
+
         return true;
     }
 
     public void stopThread() {
+
+        mGameState.stopEverything();
 
         try {
             mThread.join();
@@ -49,7 +65,15 @@ class GameEngine extends SurfaceView implements Runnable {
 
     public void startThread() {
 
+        mGameState.startThread();
+
         mThread = new Thread(this);
         mThread.start();
+    }
+
+    public void deSpawnReSpawn() {
+        // Eventually this will despawn
+        // and then respawn all the game objects
+
     }
 }
---
+++ ScrollingShooter/app/src/main/java/com/aruyamam/scrollingshooter/GameStarter.java
@@ -0,0 +1,9 @@
+package com.aruyamam.scrollingshooter;
+
+interface GameStarter {
+
+    // This allows the State class to
+    // spawn and despawn objects via the game engine
+
+    public void deSpawnReSpawn();
+}
