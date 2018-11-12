0de16dc41a1e336f9d01d43da612df8455eca20c
aruyamam <aruyamam@gmail.com>
2018-11-12 19:53:57 +0900

    Completing the background's component

--- ScrollingShooter/app/src/main/java/com/aruyamam/scrollingshooter/BackgroundSpawnComponent.java
+++ ScrollingShooter/app/src/main/java/com/aruyamam/scrollingshooter/BackgroundSpawnComponent.java
@@ -4,6 +4,7 @@ class BackgroundSpawnComponent implements SpawnComponent {
 
     @Override
     public void spawn(Transform playerTransform, Transform t) {
-
+        // Place the background in the top left corner
+        t.setLocation(0f, 0f);
     }
 }
